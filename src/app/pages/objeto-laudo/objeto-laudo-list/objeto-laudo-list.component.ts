c98c3a83aece3f7171a1afa87e6d2adc680f53ab
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-04-13 22:04:36 -0300

    Refs. #LP-31 - Implementação da caixa de diálogo para edição

--- src/app/pages/objeto-laudo/objeto-laudo-list/objeto-laudo-list.component.ts
+++ src/app/pages/objeto-laudo/objeto-laudo-list/objeto-laudo-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ObjetoLaudo } from '../shared/objeto-laudo.model';
 import { ErrorHandlerService } from '../../../core/error-handler.service';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { ObjetoLaudoService } from '../shared/objeto-laudo.service';
+import { Documento } from '../../documentos/shared/documento.model';
 
 @Component({
   selector: 'app-objeto-laudo-list',
@@ -16,13 +17,19 @@ export class ObjetoLaudoListComponent implements OnInit {
   @Input() titulo: string = 'Título Exemplo';
 
   objeto = new ObjetoLaudo();
+  documento = new Documento();
+
   objetos!: ObjetoLaudo[];
   objetosSelecionados!: ObjetoLaudo[] | null;
+
+  objetoDialog: boolean = false;
+  submitted: boolean = false;
 
   constructor(
     private error: ErrorHandlerService,
     private msgService: MessageService,
-    private objetoService: ObjetoLaudoService
+    private objetoService: ObjetoLaudoService,
+    private confirmacaoService: ConfirmationService
   ) { }
 
   ngOnInit(): void {
@@ -42,7 +49,18 @@ export class ObjetoLaudoListComponent implements OnInit {
   }
 
   deletarObjetosSelecionados() {
-    throw new Error('Method not implemented.');
+    this.confirmacaoService.confirm({
+      message: 'Tem certeza em deletar os objetos selecionados?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+        this.objetos = this.objetos.filter((value) => !this.objetosSelecionados?.includes(value));
+        this.objetosSelecionados = [];
+        this.msgService.add({ severity: 'success', summary: 'Sucesso', detail: 'Objetos apagados', life: 3000 });
+      }
+    });
   }
 
   applyFilterGlobal(event: Event) {
@@ -53,11 +71,46 @@ export class ObjetoLaudoListComponent implements OnInit {
   }
 
   deletar(obj: ObjetoLaudo) {
-    throw new Error('Method not implemented.');
-    }
-    editar(obj: ObjetoLaudo) {
-    throw new Error('Method not implemented.');
-    }
+    this.confirmacaoService.confirm({
+      message: 'Tem certeza em deletar o objeto: ' + obj.documento?.nomeTitulo + '?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+
+        if (obj.id !== undefined) {
+
+          this.objetoService.excluir(obj.id)
+            .then(() => {
+              this.objetos = this.objetos.filter((value) => value.id !== obj.id);
+              this.objeto = {};
+              this.msgService.add({
+                severity: 'success', summary: 'Sucesso', detail: 'Nomeação apagada', life: 3000
+              });
+            })
+            .catch((erro) => {
+              this.error.handle(erro);
+              this.msgService.add({
+                severity: 'error', summary: 'Erro', detail: 'Erro ao excluir o objeto', life: 3000
+              });
+            })
+        } else {
+          console.error('ID de objeto é undefined');
+        }
+      }
+    });
+  }
+
+  editar(obj: ObjetoLaudo) {
+    this.objeto = { ...obj };
+    this.objetoDialog = true;
+  }
+
+  esconderDialog() {
+    this.objetoDialog = false;
+    this.submitted = false;
+  }
 
   private handleError(erro: any): void {
     this.error.handle(erro);
@@ -65,4 +118,28 @@ export class ObjetoLaudoListComponent implements OnInit {
       ({ severity: 'error', summary: 'Erro', detail: 'Ocorreu um erro ao carregar os objetos. Por favor, tente novamente mais tarde.' })
     )
   }
+
+  salvarEdicao() {
+    this.submitted = true;
+
+    this.objetoService.atualizar(this.objeto).subscribe(
+      (objetoAtualizado: ObjetoLaudo) => {
+        const index = this.objetos.findIndex(obj => obj.id === objetoAtualizado.id);
+
+        if (index !== -1) {
+          this.objetos[index] = objetoAtualizado;
+        }
+
+        this.msgService.add({
+          severity: 'success', summary: 'Sucesso', detail: 'Objeto Atualizado', life: 3000
+        });
+        this.esconderDialog();
+      },
+      erro => {
+        this.error.handle(erro);
+        console.error('Ops! Erro ao atualizar o objeto: ', erro);
+      }
+    )
+  }
+
 }
