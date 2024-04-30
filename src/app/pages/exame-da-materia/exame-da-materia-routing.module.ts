ae57bf8374fd6003cf12bdcbeb006d3e3e6cb16d
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-04-29 22:40:08 -0300

    Refs. #LP-32 - Ajustes nos componentes Exame e Objeto do Laudo

--- src/app/pages/exame-da-materia/exame-da-materia-routing.module.ts
+++ src/app/pages/exame-da-materia/exame-da-materia-routing.module.ts
@@ -6,7 +6,7 @@ import { ExameFormComponent } from './exame-form/exame-form.component';
 const routes: Routes = [
   { path: 'exames', component: ExameListComponent },
   { path: 'exames/novo', component: ExameFormComponent },
-  { path: 'exames/edit/:id', component: ExameFormComponent }
+  { path: 'exames/:id/edit', component: ExameFormComponent }
 ];
 
 @NgModule({
