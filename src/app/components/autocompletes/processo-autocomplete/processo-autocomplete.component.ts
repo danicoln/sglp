f746c57a49ba228fd38e84da5a5ca888016d1541
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-05-16 23:27:24 -0300

    Refs. #LP-53 - Ajustes na página de Laudo Pericial, definido o componente autocomplete de processo

--- src/app/components/autocompletes/processo-autocomplete/processo-autocomplete.component.ts
+++ src/app/components/autocompletes/processo-autocomplete/processo-autocomplete.component.ts
@@ -6,64 +6,106 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormBuilder, FormGroup } from 
 @Component({
   selector: 'app-processo-autocomplete',
   template: `
-    <div style="display: flex;">
-      <p-card class="col-6 md:col-6" [style]="{'height':'270px'}">
-        <div class="title">
-          <b >{{ title }}</b>
-        </div>
-        <p-autoComplete
-          [(ngModel)]="value"
-          [suggestions]="filtroProcessos"
-          (completeMethod)="search($event)"
-          field="numero"
-          (processoSelecionado)="onProcessoSelecionado(value)"
-          (ngModelChange)="onProcessoChange(value)"
-          (onBlur)="onTouch()"
-          >
-        </p-autoComplete>
-        <div class="row mt-2">
-          <a
-            href="/processos/novo"
-            pButton
-            class="p-button"
-            label="Novo Processo"
-            style="text-decoration: none;"
-            >
-            <i style="cursor: pointer;" class="pi pi-plus mr-1"></i>
-          </a>
+    <div>
+      <p-card class="col-12 md:col-6">
+        <section class="row processo-card" *ngIf="!processoConfirmado">
+
+          <div class="col-6 processo-input">
+
+            <div class="autocomplete">
+              <div class="title">
+                <b >{{ title }}</b>
+              </div>
+                <p-autoComplete
+                  [(ngModel)]="value"
+                  [suggestions]="filtroProcessos"
+                  (completeMethod)="search($event)"
+                  field="numero"
+                  (processoSelecionado)="onProcessoSelecionado(value)"
+                  (ngModelChange)="onProcessoChange(value)"
+                  (onBlur)="onTouch()"
+                  >
+                </p-autoComplete>
+                <div *ngIf="valorInvalido">
+                  <small class="text-danger">Insira um número de processo válido</small>
+                </div>
+            </div>
+
+            <div class="col-6 btn-confirmar">
+              <div class="mt-2">
+
+                <button
+                  pButton
+                  class="pi pi-check"
+                  (click)="confirmar()"
+                  >
+                </button>
+
+              </div>
+            </div>
+          </div>
+
+
+        </section>
+
+        <div class="col-12 md:col-6 mb-2" *ngIf="value && processoConfirmado">
+          <div class="dadosProcesso-r">
+            <div class="elemento" style="display: flex;">
+              <p class="n-processo ml-2"><strong>Processo: </strong>{{value.numero}}</p>
+            </div>
+            <div class="elemento" style="display: flex;">
+              <h4></h4>
+              <p class="assunto ml-2"><strong>Assunto: </strong>{{value.assunto}}</p>
+            </div>
+            <div class="elemento" style="display: flex;">
+              <p class="nomeAutor ml-2"><strong>Autor: </strong>{{value.nomeAutor}}</p>
+            </div>
+            <div class="elemento" style="display: flex;">
+              <p class="nomeReu ml-2"><strong>Réu: </strong>{{value.nomeReu}}</p>
+            </div>
+          </div>
+
+          <div class="btn-alterar">
+            <button
+              pButton
+              class="pi pi-angle-double-left"
+              (click)="alterarProcesso()"
+              >
+            </button>
+          </div>
 
         </div>
       </p-card>
-      <div class="p-card col-6 md:col-6 mb-2" *ngIf="value && processoConfirmado" style="display: flex;">
-        <div class="dadosProcesso-r">
-          <div class="elemento" style="display: flex;">
-            <p class="n-processo ml-2"><strong>Processo: </strong>{{value.numero}}</p>
-          </div>
-          <div class="elemento" style="display: flex;">
-            <h4></h4>
-            <p class="assunto ml-2"><strong>Assunto: </strong>{{value.assunto}}</p>
-          </div>
-          <div class="elemento" style="display: flex;">
-            <p class="nomeAutor ml-2"><strong>Autor: </strong>{{value.nomeAutor}}</p>
-          </div>
-          <div class="elemento" style="display: flex;">
-            <p class="nomeReu ml-2"><strong>Réu: </strong>{{value.nomeReu}}</p>
-          </div>
-        </div>
-
-      </div>
     </div>
   `,
   styles: [
     `
+    .btn-confirmar, .btn-alterar {
+      margin-top: 6px;
+    }
+
+    .btn-alterar{
+      margin-top: auto;
+    }
+
     .container-r {
       display:flex;
       flex-direction:row;
       justify-content:flex-end;
     }
 
-    .dadosProcesso {
+    .processo-card, .processo-input {
+      display:flex;
+    }
+
+    .dadosProcesso, .dadosProcesso-r {
       display:block;
+    }
+
+    .text-danger{
+      background-color: #ff3535c7;
+      color: white;
+      font-weight: bold;
     }
     `
   ],
@@ -79,13 +121,14 @@ export class ProcessoAutocompleteComponent implements ControlValueAccessor, OnDe
 
   @Input() title!: string;
   @Input() displayField!: string;
+  @Input() valorInvalido: boolean = false;
+  @Input() processoConfirmado: boolean = false;
+  @Input() processo!: Processo;
   @Output() processoSelecionado = new EventEmitter<string>();
 
   processos!: Processo[];
   filtroProcessos!: Processo[];
-  processoConfirmado: boolean = false;
 
-  private processo?: Processo;
 
   formGroup!: FormGroup;
 
@@ -121,7 +164,7 @@ export class ProcessoAutocompleteComponent implements ControlValueAccessor, OnDe
   }
 
   writeValue(value: any): void {
-    if(value) {
+    if (value) {
       this.formGroup.setValue(value);
     }
   }
@@ -135,32 +178,28 @@ export class ProcessoAutocompleteComponent implements ControlValueAccessor, OnDe
   }
 
   ngOnDestroy() {
-    if(this.onChange && typeof this.onChange.unsubscribe === 'function'){
+    if (this.onChange && typeof this.onChange.unsubscribe === 'function') {
       this.onChange.unsubscribe();
     }
   }
 
   setDisabledState(isDisabled: boolean): void {
-      if(isDisabled) {
-        this.formGroup.disable();
-      } else {
-        this.formGroup.enable();
-      }
+    if (isDisabled) {
+      this.formGroup.disable();
+    } else {
+      this.formGroup.enable();
+    }
   }
 
-  onProcessoSelecionado(processo: any): void {
-    console.log('Autocomplete: chama onSelectProcesso: ', processo)
+  onProcessoSelecionado(processo: any) {
     this.processoSelecionado.emit(processo);
   }
 
 
   onProcessoChange(value: any) {
-
     this.processoConfirmado = false;
     this.processo = value;
     this.formGroup.get('processo')?.setValue(value)
-
-    console.log('Autocomplete -> onProcessoChange: ', this.processo)
   }
 
   get value() {
@@ -168,10 +207,26 @@ export class ProcessoAutocompleteComponent implements ControlValueAccessor, OnDe
   }
 
   set value(value: any) {
-    if(value !== this.processo) {
+    if (value !== this.processo) {
       this.processo = value;
       this.onChange = value;
     }
+  }
+
+  confirmar() {
+    const processo = this.formGroup.get('processo')?.value;
+
+    if (!processo) {
+      return;
+    }
+    this.valorInvalido = true;
+    this.processoConfirmado = true;
+    this.processoSelecionado.emit(processo);
+  }
+
+  alterarProcesso() {
+    this.valorInvalido = false;
+    this.processoConfirmado = false;
   }
 
 
